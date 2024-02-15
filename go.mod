75d160286e309a69aec56b146a1b8cb91c26da24
tod1az <t.diaz.soto@gmail.com>
2024-02-15 17:16:28 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tod1az/go-intro.git
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var result string =	retunrsomething("XD")
+	fmt.Println(result)
+}
+
+func printMe(value string){
+	fmt.Println(value)
+}
+func retunrsomething(value string) string{
+	return value +"XD"
+}
